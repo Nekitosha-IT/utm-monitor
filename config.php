5b67f36c238c307334387095f3f8e1d2d5fb4212
Nekitosha-IT <mrvendettabest@gmail.com>
2026-09-09 00:00:22 +0300

    Reduce UTM timeout to keep monitoring responsive

--- config.php
+++ config.php
@@ -4,7 +4,8 @@ return [
 
     'refresh' => 600,
 
-    'timeout' => 3,
+    // UTM is on the LAN; a failed endpoint must not freeze the dashboard.
+    'timeout' => 1,
 
     'storage' => [
         'file' => __DIR__ . '/utms.json'
@@ -20,77 +21,15 @@ return [
             'enabled' => true
         ],
 
-        2 => [
-            'id' => 2,
-            'name' => 'УТМ №2',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ],
-
-        3 => [
-            'id' => 3,
-            'name' => 'УТМ №3',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ],
-
-        4 => [
-            'id' => 4,
-            'name' => 'УТМ №4',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ],
-
-        5 => [
-            'id' => 5,
-            'name' => 'УТМ №5',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ],
-
-        6 => [
-            'id' => 6,
-            'name' => 'УТМ №6',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ],
-
-        7 => [
-            'id' => 7,
-            'name' => 'УТМ №7',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ],
-
-        8 => [
-            'id' => 8,
-            'name' => 'УТМ №8',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ],
-
-        9 => [
-            'id' => 9,
-            'name' => 'УТМ №9',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ],
-
-        10 => [
-            'id' => 10,
-            'name' => 'УТМ №10',
-            'ip' => '',
-            'port' => 8086,
-            'enabled' => false
-        ]
+        2 => ['id'=>2,'name'=>'УТМ №2','ip'=>'','port'=>8086,'enabled'=>false],
+        3 => ['id'=>3,'name'=>'УТМ №3','ip'=>'','port'=>8086,'enabled'=>false],
+        4 => ['id'=>4,'name'=>'УТМ №4','ip'=>'','port'=>8086,'enabled'=>false],
+        5 => ['id'=>5,'name'=>'УТМ №5','ip'=>'','port'=>8086,'enabled'=>false],
+        6 => ['id'=>6,'name'=>'УТМ №6','ip'=>'','port'=>8086,'enabled'=>false],
+        7 => ['id'=>7,'name'=>'УТМ №7','ip'=>'','port'=>8086,'enabled'=>false],
+        8 => ['id'=>8,'name'=>'УТМ №8','ip'=>'','port'=>8086,'enabled'=>false],
+        9 => ['id'=>9,'name'=>'УТМ №9','ip'=>'','port'=>8086,'enabled'=>false],
+        10 => ['id'=>10,'name'=>'УТМ №10','ip'=>'','port'=>8086,'enabled'=>false]
 
     ]
 
