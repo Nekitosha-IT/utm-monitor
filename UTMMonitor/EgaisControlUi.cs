ec64403b769da994066ca40ed455f188928d566b
Nekitosha-IT <mrvendettabest@gmail.com>
2026-09-09 03:29:21 +0300

    feat: add EGAIS control menu

---
+++ UTMMonitor/EgaisControlUi.cs
@@ -0,0 +1,22 @@
+namespace UTMMonitor;
+
+public static class EgaisControlUi
+{
+    public static void Install(Form form)
+    {
+        var menu = form.MainMenuStrip;
+        if (menu == null) return;
+        var egais = new ToolStripMenuItem("ЕГАИС");
+        egais.DropDownItems.Add("Центр контроля ЕГАИС…", null, (_, _) =>
+        {
+            using var f = new EgaisControlForm();
+            f.ShowDialog(form);
+        });
+        egais.DropDownItems.Add("Массовая проверка акцизных марок…", null, (_, _) =>
+        {
+            using var f = new MarkBatchForm();
+            f.ShowDialog(form);
+        });
+        menu.Items.Add(egais);
+    }
+}
